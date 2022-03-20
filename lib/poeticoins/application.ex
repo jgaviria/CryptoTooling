6155f9a90e5aa580eee4034336009851704f3b49
Juan Gaviria <gaviria08111@gmail.com>
2022-03-20 19:59:42 -0400

    Add dynamic supervisors (one per client)

--- lib/poeticoins/application.ex
+++ lib/poeticoins/application.ex
@@ -1,8 +1,5 @@
+#lib/poeticoins/application.ex
 defmodule Poeticoins.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   def start(_type, _args) do
@@ -11,6 +8,10 @@ defmodule Poeticoins.Application do
       PoeticoinsWeb.Telemetry,
       # Start the PubSub system
       {Phoenix.PubSub, name: Poeticoins.PubSub},
+
+      {Poeticoins.Historical, name: Poeticoins.Historical},
+      {Poeticoins.Exchanges.Supervisor, name: Poeticoins.Exchanges.Supervisor},
+
       # Start the Endpoint (http/https)
       PoeticoinsWeb.Endpoint
       # Start a worker by calling: Poeticoins.Worker.start_link(arg)
@@ -23,10 +24,4 @@ defmodule Poeticoins.Application do
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  def config_change(changed, _new, removed) do
-    PoeticoinsWeb.Endpoint.config_change(changed, removed)
-    :ok
-  end
 end
