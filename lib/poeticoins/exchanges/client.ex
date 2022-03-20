2c0190943f3bc9bea76f60f2dc32b00be5f74c12
Juan Gaviria <gaviria08111@gmail.com>
2022-03-19 23:58:26 -0400

    Using macros to simplify client creation

--- lib/poeticoins/exchanges/client.ex
+++ lib/poeticoins/exchanges/client.ex
@@ -1,4 +1,3 @@
-#lib/poeticoins/exchanges/client.ex
 defmodule Poeticoins.Exchanges.Client do
   use GenServer
 
@@ -9,6 +8,7 @@ defmodule Poeticoins.Exchanges.Client do
                currency_pairs: [String.t()]
              }
 
+
   @callback exchange_name() :: String.t()
   @callback server_host() :: list()
   @callback server_port() :: integer()
@@ -17,7 +17,32 @@ defmodule Poeticoins.Exchanges.Client do
 
   defstruct [:module, :conn, :conn_ref, :currency_pairs]
 
-  def start_link(module, currency_pairs, options \\ []) do
+  defmacro defclient(options) do
+    exchange_name = Keyword.fetch!(options, :exchange_name)
+    host = Keyword.fetch!(options, :host)
+    port = Keyword.fetch!(options, :port)
+    currency_pairs = Keyword.fetch!(options, :currency_pairs)
+
+    quote do
+      @behaviour unquote(__MODULE__)
+      import unquote(__MODULE__), only: [validate_required: 2]
+      require Logger
+
+      def exchange_name, do: unquote(exchange_name)
+      def server_host, do: unquote(host)
+      def server_port, do: unquote(port)
+      def available_currency_pairs, do: unquote(currency_pairs)
+
+      def handle_ws_message(msg, client) do
+        Logger.debug("handle_ws_message: #{inspect(msg)}")
+        {:noreply, client}
+      end
+
+      defoverridable [handle_ws_message: 2]
+    end
+  end
+
+  def start_link(module, currency_pairs, options \\[]) do
     GenServer.start_link(__MODULE__, {module, currency_pairs}, options)
   end
 
@@ -41,27 +66,25 @@ defmodule Poeticoins.Exchanges.Client do
     %{client | conn: conn, conn_ref: conn_ref}
   end
 
-  defp server_host(module), do: module.server_host()
 
-  defp server_port(module), do: module.server_port()
-
-  def handle_info({:gun_up, conn, :http}, %{conn: conn} = client) do
+  def handle_info({:gun_up, conn, :http}, %{conn: conn}=client) do
     :gun.ws_upgrade(conn, "/")
     {:noreply, client}
   end
 
-  def handle_info(
-        {:gun_upgrade, conn, _ref, ["websocket"], _headers},
-        %{conn: conn} = client
-      )
+  def handle_info({:gun_upgrade, conn, _ref, ["websocket"], _headers},
+        %{conn: conn}=client)
     do
     subscribe(client)
     {:noreply, client}
   end
 
-  def handle_info({:gun_ws, conn, _ref, {:text, msg} = _frame}, %{conn: conn} = client) do
+  def handle_info({:gun_ws, conn, _ref, {:text, msg}=_frame}, %{conn: conn}=client) do
     handle_ws_message(Jason.decode!(msg), client)
   end
+
+  defp server_host(module), do: module.server_host()
+  defp server_port(module), do: module.server_port()
 
 
   defp subscribe(client) do
