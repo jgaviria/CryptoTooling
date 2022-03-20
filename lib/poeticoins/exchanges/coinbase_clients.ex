8399164db4d44afe43618a9fd0e4fbd0a9e0b003
Juan Gaviria <gaviria08111@gmail.com>
2022-03-19 23:00:25 -0400

    both bitstam and conbase clients with separate genservers

---
+++ lib/poeticoins/exchanges/coinbase_clients.ex
@@ -0,0 +1,114 @@
+defmodule Poeticoins.Exchanges.CoinbaseClient do
+  use GenServer
+  alias Poeticoins.{Trade, Product}
+  @exchange_name "coinbase"
+
+  def start_link(currency_pairs, options \\ []) do
+    GenServer.start_link(__MODULE__, currency_pairs, options)
+  end
+
+  def init(currency_pairs) do
+    state = %{
+      currency_pairs: currency_pairs,
+      conn: nil
+    }
+    {:ok, state, {:continue, :connect}}
+  end
+
+  def handle_continue(:connect, state) do
+    updated_state = connect(state)
+    {:noreply, updated_state}
+  end
+
+  def server_host, do: 'ws-feed.pro.coinbase.com'
+  def server_port, do: 443
+
+  def connect(state) do
+    {:ok, conn} = :gun.open(server_host(), server_port(), %{protocols: [:http]})
+    %{state | conn: conn}
+  end
+
+  def handle_info({:gun_up, conn, :http}, %{conn: conn} = state) do
+    :gun.ws_upgrade(state.conn, "/")
+    {:noreply, state}
+  end
+
+  def handle_info(
+        {:gun_upgrade, conn, _ref, ["websocket"], _headers},
+        %{conn: conn} = state
+      )
+    do
+    subscribe(state)
+    {:noreply, state}
+  end
+
+  # Handle messages
+  def handle_info(
+        {:gun_ws, conn, _ref, {:text, msg} = _frame},
+        %{conn: conn} = state
+      )
+    do
+    handle_ws_message(Jason.decode!(msg), state)
+  end
+
+  defp subscribe(state) do
+    # subscription frames
+    # send subscription frames to coinbase
+    subscription_frames(state.currency_pairs)
+    |> Enum.each(&:gun.ws_send(state.conn, &1))
+  end
+
+  def subscription_frames(currency_pairs) do
+    msg = %{
+            "type" => "subscribe",
+            "product_ids" => currency_pairs,
+            "channels" => ["ticker"]
+          }
+          |> Jason.encode!()
+
+    [{:text, msg}]
+  end
+
+  # work horse
+  def handle_ws_message(%{"type" => "ticker"} = msg, state) do
+    _trade =
+      msg
+      |> message_to_trade()
+      |> IO.inspect(label: "trade")
+
+    {:noreply, state}
+  end
+
+  def handle_ws_message(msg, state) do
+    IO.inspect(msg, label: "unhandled message")
+    {:noreply, state}
+  end
+
+  @spec message_to_trade(map()) :: {:ok, Trade.t()} | {:error, any()}
+  def message_to_trade(msg) do
+    with :ok <- validate_required(msg, ["product_id", "time", "price", "last_size"]),
+         {:ok, traded_at, _} <- DateTime.from_iso8601(msg["time"])
+      do
+      currency_pair = msg["product_id"]
+
+      trade = Trade.new(
+        product: Product.new(@exchange_name, currency_pair),
+        price: msg["price"],
+        volume: msg["last_size"],
+        traded_at: traded_at
+      )
+
+      {:ok, trade}
+    else
+      {:error, _reason} = error -> error
+    end
+  end
+
+  @spec validate_required(map(), [String.t()]) :: :ok | {:error, {String.t(), :required}}
+  def validate_required(msg, keys) do
+    required_key = Enum.find(keys, fn k -> is_nil(msg[k]) end)
+
+    if is_nil(required_key), do: :ok,
+                             else: {:error, {required_key, :required}}
+  end
+end
---
+++ lib/poeticoins/product.ex
@@ -0,0 +1,18 @@
+# lib/poeticoins/product.ex
+defmodule Poeticoins.Product do
+  defstruct [:exchange_name, :currency_pair]
+
+  @type t() :: %__MODULE__{
+                 exchange_name: String.t(),
+                 currency_pair: String.t()
+               }
+  @spec new(String.t(), String.t()) :: t()
+  def new(exchange_name, currency_pair) do
+    %__MODULE__{
+      exchange_name: exchange_name,
+      currency_pair: currency_pair
+    }
+  end
+
+
+end
